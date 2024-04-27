eb5b3825ba8ad44158755d2ccdb2b8ed0ddde924
XQY279 <xiangqingyuan313@icloud.com>
2024-04-27 11:54:13 +0800

    feat: commit

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -3,7 +3,7 @@ import { useState } from "react";
 export const useCounter = (initialValue = 0) => {
   const [value, setValue] = useState(initialValue);
 
-  const increment = () => setValue(value + 2);
+  const increment = () => setValue(value + 1);
   const decrement = () => setValue(value - 1);
 
   return { value, increment, decrement };
